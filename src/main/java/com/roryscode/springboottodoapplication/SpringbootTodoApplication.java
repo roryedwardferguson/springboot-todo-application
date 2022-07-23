b3f2bc08c65afc367e4764b1336c6229f3da6dde
Rory Ferguson <60031008+roryedwardferguson@users.noreply.github.com>
2022-07-23 17:10:52 +0100

    Initial commit

---
+++ src/main/java/com/roryscode/springboottodoapplication/SpringbootTodoApplication.java
@@ -0,0 +1,13 @@
+package com.roryscode.springboottodoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roryscode/springboottodoapplication/SpringbootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roryscode.springboottodoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

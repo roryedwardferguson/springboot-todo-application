323a973c4ec08a433d84e39eb1edca8a550114f8
Rory Ferguson <60031008+roryedwardferguson@users.noreply.github.com>
2022-07-25 14:17:24 +0100

    Autowired the TodoItemRepository and invoked the addObject method with attribute name and attribute value

--- src/main/java/com/roryscode/springboottodoapplication/controllers/TodoItemController.java
+++ src/main/java/com/roryscode/springboottodoapplication/controllers/TodoItemController.java
@@ -1,7 +1,9 @@
 package com.roryscode.springboottodoapplication.controllers;
 
+import com.roryscode.springboottodoapplication.repositories.TodoItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,10 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
 
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
     @GetMapping("/")
     public ModelAndView index(){
         logger.debug("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
         return modelAndView;
     }
 }
